1b913f2452d799f9448a23a5cb287166565f4ff8
nordluma <maxnordlund.95@hotmail.com>
2023-03-12 22:04:46 +0200

    New: Create note now creates a folder and opens the folder in file explorer

--- src/notes/notes.rs
+++ src/notes/notes.rs
@@ -1,3 +1,5 @@
+use std::{fs::create_dir_all, path::Path, process::Command};
+
 use crate::args::{CreateNote, DeleteNote, NoteCommand, NoteSubcommand, OpenNote};
 
 pub fn handle_note_command(note: NoteCommand) {
@@ -12,7 +14,17 @@ pub fn handle_note_command(note: NoteCommand) {
 
 fn create_note(note: CreateNote) {
     println!("Creating new note: {:?}", note);
-    todo!("Create new Note")
+    // TODO: Read from user config path where the note should be created
+    let new_path = format!("C:/testfolder/{}", note.name);
+    if !Path::new(&new_path).exists() {
+        create_dir_all(&new_path).expect("Couldn't create the folder");
+    }
+
+    let cmd_to_execute = format!("start {}", new_path);
+    Command::new("cmd")
+        .args(["/C", cmd_to_execute.as_str()])
+        .spawn()
+        .expect("failed to execute process");
 }
 
 fn open_note(note: OpenNote) {
